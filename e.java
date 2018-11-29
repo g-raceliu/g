5d2efaa7495a8ae64947de62be46c59b6fa69c8e
Grace Liu <lyuhan@gmail.com>
2018-11-29 20:34:05 +0000

    Message

---
+++ e.java
@@ -0,0 +1,8 @@
+ls - view everything in current folder
+cd - change folder [directory]
+cd.. - go back one folder
+git clone [link] - brings words on github onto here
+git status - shows changes
+git add . - adds all changes to the "stage"
+git commit -m "MESSAGE" = commits all changes 
+git push - pushes all of your commits to online
